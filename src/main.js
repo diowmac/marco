2cb498743f9548ab91b16f576a67daba58340139
marco ezequiel <marcoezequielcedro@gmail.com>
2017-10-16 14:00:24 -0200

    Primeiro commit de teste

--- src/main.js
+++ src/main.js
@@ -9,13 +9,20 @@ new Vue({
     data: {
         times:[
             new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
-            new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
-            new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
-            new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
-            new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
-            new Time( 'America MG' , require('./assets/america_mg_60x60.png') ),
+            new Time( 'Atletico PR' , require('./assets/atletico-pr_60x60.png') ),
+            new Time( 'atletico MG' , require('./assets/atletico_mg_60x60.png') ),
+            new Time( 'Botafogo' , require('./assets/botafogo_60x60.png') ),
+            new Time( 'Chapecoense' , require('./assets/chapecoense_60x60.png') ),
+            new Time( 'Corinthians' , require('./assets/corinthians_60x60.png') ),
 
-        ]
+        ],
+        alfabeto: {
+            a: 'A',
+            b: 'B',
+            c: 'C',
+            d: 'D',
+            e: 'E',
+        }
 
     },
 });
